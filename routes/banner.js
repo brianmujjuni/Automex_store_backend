2c611b22bab114ec818b9d46f1025e183aa8515f
brianmujjuni <brianmujjuni@engineer.com>
2025-01-21 02:03:57 +0300

    resolve req bug

--- routes/banner.js
+++ routes/banner.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Banner = require("../models/banner");
 const bannerRouter = express.Router();
 
-bannerRouter.post("/api/banner", async (requestAnimationFrame, res) => {
+bannerRouter.post("/api/banner", async (req, res) => {
   try {
     const { image } = req.body;
     const banner = new Banner({ image });
