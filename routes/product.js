de4817e54c55e1cf719f5b754b43266f07f8ff08
brianmujjuni <brianmujjuni@engineer.com>
2025-06-18 18:18:58 +0300

    return popular products as object

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ const Product = require("../models/product");
 const productRouter = express.Router();
 
 productRouter.post("/api/add-product", async (req, res) => {
+  console.log(req)
   try {
     const {
       vendorId,
@@ -42,7 +43,7 @@ productRouter.get("/api/popular-products", async (req, res) => {
     if (!product || product.length == 0) {
       return res.status(404).json({ msg: "Products not found" });
     }
-    return res.status(200).json({ product });
+    return res.status(200).json( product );
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
